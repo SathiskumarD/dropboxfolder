4b256501b7eec05650305991deff127914c923fe
Sathiskumar <Sathiskumar.Dharmalingam@outlook.com>
2020-05-25 14:52:04 -0500

    commit

---
+++ Original/12app.js
@@ -0,0 +1,27 @@
+// let a = 10;
+// let b = '25';
+// console.log(a + b);
+// let c = '20 hello';
+// console.log(a + c);
+// b++;
+// //c++;
+// let d = Number(b);
+// console.log(d);
+// console.log(Number(c));
+// console.log(parseInt(c));
+
+// let a = 10;
+// let b = "25";
+// console.log(a + b);
+let c = "20 hello";
+// console.log(a + c);
+// b++; //JavaScript can convert string to integer//
+// console.log(b);
+// c++;
+// console.log(c);
+// let d = Number(b);
+// console.log(d);
+// let e = Number(c);
+// console.log(e);
+// console.log(parseInt(b)); //first time learning parseInt
+console.log(parseInt(c));
---
+++ Original/13app.js
@@ -0,0 +1,22 @@
+// let greeting = 'Hello World';
+// let js = 'JavaScript';
+// let mes1 = 'i\'d rather be coding '+js;
+// let mes2 = `I'd rather be coding ${js}`;
+// let mes3 = `
+// <h1>${js}</h1>
+// <p>${mes1}</p>
+// <p>${mes2}</p>
+// `;
+// console.log(mes3);
+
+let greeting = "Hello";
+let js = 'JavaScript';
+let mes1 = 'i\'d rather be coding ' + js;
+let mes2 = `I'\d rather be coding ${js}`; //using backtick
+let mes3 = `
+<h1>${js}<h1>
+<h2>Welcome<h2>
+`
+console.log(mes1);
+console.log(mes2);
+console.log(mes3);
