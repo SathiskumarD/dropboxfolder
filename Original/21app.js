f016aff531dae50b3958fd5cbf3ec11d65532705
Sathiskumar <Sathiskumar.Dharmalingam@outlook.com>
2020-06-02 16:56:32 -0500

    functions

---
+++ Original/21app.js
@@ -0,0 +1,30 @@
+// message2();
+// const message1 = function () {
+//     console.log('Hello world function expression');
+// }
+// message1();
+// message1();
+// message1();
+
+// function message2() {
+//     console.log('Hello world function declaration');
+// }
+// message2();
+// message2();
+// message2();
+
+message2();
+//message1();
+
+//message1 will not work because it is not initialized yet.However, message2 works.
+
+const message1 = function () {
+    console.log("Hello world function expression");
+}
+message1();
+
+function message2() {
+    console.log("Hello world function declaration");
+}
+
+// mes
