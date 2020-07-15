d1da1dfbae292f3674bf871724dc262ebaff68fe
Sathiskumar <Sathiskumar.Dharmalingam@outlook.com>
2020-07-14 19:48:19 -0500

    commit

--- VanillaJSwithHTMLelements/playlist.js
+++ VanillaJSwithHTMLelements/playlist.js
@@ -1,8 +1,24 @@
-window.onload=init;
-function init(){
-var button = document.getElementById("addButton");
-button.onclick = handleButtonClick;
+window.onload = init;
+
+function init() {
+    document.getElementById("addButton").onclick = handleButtonClick;
+    loadPlaylist();
 }
-function handleButtonClick(){
-    alert("Button was clicked")
+
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput");
+    //var songName = textInput.value;
+
+    if (textInput.value == "") {
+        alert("please enter a song")
+    } else {
+        alert("Adding  " + textInput.value);
+    }
+
+    var li = document.createElement("li");
+    var ul = document.getElementById("playlist");
+    li.innerHTML = textInput.value;
+    ul.appendChild(li);
+    save(songname);
+
 }
---
+++ VanillaJSwithHTMLelements/playlist1.js
@@ -0,0 +1,12 @@
+window.onload = one;
+
+/* function buttonOnClick() {
+
+    document.getElementById("addButton").onclick = function () {
+        one()
+    };
+} */
+
+function one() {
+    alert("Hello Someone is Trying Add song(s)!");
+}
