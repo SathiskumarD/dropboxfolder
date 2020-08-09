b4271a1103634f03d945f1cb1d83373e26543f62
Sathiskumar <Sathiskumar.Dharmalingam@outlook.com>
2020-08-09 12:58:51 -0500

    changes

--- VanillaJSwithHTMLelements/skillsoft/datatypes.js
+++ VanillaJSwithHTMLelements/skillsoft/datatypes.js
@@ -4,9 +4,13 @@ console.log(typeof x);
 let y=true;//can be true or false//
 console.log(typeof y);
 
-var z='hellow world'//single quotes//
+var z='hello world'//single quotes//
 console.log(typeof z);*/
 
 let a;
 console.log(a);
 console.log(typeof a);
+const firstName = "Sathishkumar";
+const lastName = "Dharmalingam";
+val = firstName.slice(0, 4);
+console.log(val);
