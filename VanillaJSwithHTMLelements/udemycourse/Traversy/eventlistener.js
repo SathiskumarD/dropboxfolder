372c8abf099915bee529a8ce303c03f610759f5d
Sathiskumar <Sathiskumar.Dharmalingam@outlook.com>
2020-08-29 08:16:40 -0500

    changes

--- VanillaJSwithHTMLelements/udemycourse/Traversy/eventlistener.js
+++ VanillaJSwithHTMLelements/udemycourse/Traversy/eventlistener.js
@@ -9,12 +9,18 @@ document.querySelector(".clear-tasks").addEventListener("click", onClick);
 
 function onClick(e) {
   //console.log("clicked");
-
-  //let val;
-
   //Event target element
-  // const val1 = e.target;
+  /* const val1 = e.target;
+  console.log(val1);
+
   const val2 = e.target.id;
-  //console.log(val1);
   console.log(val2);
+  const val3 = e.target.className;
+  console.log(val3); 
+  const val4 = e.target.classList;
+  console.log(val4);*/
+  //e.target.innerText = "Hello";
+  //Event Type
+  val6 = e.type;
+  console.log(val6);
 }
