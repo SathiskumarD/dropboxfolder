5dd59e33de08312275824709cc38aa4653bc2069
sahiskumar <sahiskumar.dharmalingam@outlook.com>
2021-01-30 16:57:48 -0600

    change

--- VanillaJSwithHTMLelements/JQuery/JQuery-Practice/custom.js
+++ VanillaJSwithHTMLelements/JQuery/JQuery-Practice/custom.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   // $("div").on('click',function(){
   //   alert('DIV is clicked');
   // });
@@ -6,25 +6,27 @@ $(function(){
   // $("*").on("click",function(){
   //   alert('This is all element selector');
   // });
-// $('div').on('click',function(){
-//   alert('DIV is clicked');
-//   $(this).hide();
-// });
-$('div:eq(2)').fadeToggle('slow', function() {
-  alert("This will fade Toggle")
-});
+  // $('div').on('click',function(){
+  //   alert('DIV is clicked');
+  //   $(this).hide();
+  // });
+  $("div:eq(2)").fadeToggle("slow", function () {
+    alert("This will fade Toggle");
+  });
 
+  $("#Hw").on("click", function (event) {
+    event.preventDefault();
+    alert("Hw is clicked");
+  });
+  $(".checksome").on("click", function () {
+    $("p").hide();
+  });
+  $(".checksome").on("click", function () {
+    $(this).hide();
+  });
 
-$("#Hw").on('click',function(event){
-  event.preventDefault();
-  alert('Hw is clicked');
-});
-$(".checksome").on('click',function(){
-  $("p").hide();
-});
-$("div,.checksome").on('click',function(){
-  $(this).hide();
-});
-
-
+  $(".head:first").on("click", function () {
+    var imageURL = "sampleimage.jpg";
+    $(".box").css("background-image", "url(" + imageURL + ")");
+  });
 });
